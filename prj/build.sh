b60fd03560996f022bb6d41cb1f56d7d53c13beb
Carlos Asensio <carlos.asensiomartinez@gemalto.com>
2017-10-16 22:34:38 +0200

    change the structure

---
+++ prj/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# im too lazy to build a makefile right now
+mkdir obj
+cd obj
+
+gcc -c ../../src/convolution.c -O3  -o convolution.o
+gcc -c ../../src/image.c -O3 -o image.o
+gcc -c ../../src/bmp.c -O3 -o bmp.o
+gcc -c ../../src/canny.c -O3 -o canny.o
+gcc -c ../../src/hough.c -O3 -o hough.o
+gcc -c ../../src/timer.c -O3 -o timer.o
+gcc ../../src/main.c -O3 -o ../../bin/main timer.o hough.o canny.o image.o bmp.o convolution.o
