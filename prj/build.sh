d2911d4027e7f63ec4eda602ad1ba7063647f143
Carlos Asensio <carlos.asensiomartinez@gemalto.com>
2017-10-16 22:53:46 +0200

    Added a codeblocks project

--- prj/build.sh
+++ prj/build.sh
@@ -10,4 +10,4 @@ gcc -c ../../src/bmp.c -O3 -o bmp.o
 gcc -c ../../src/canny.c -O3 -o canny.o
 gcc -c ../../src/hough.c -O3 -o hough.o
 gcc -c ../../src/timer.c -O3 -o timer.o
-gcc ../../src/main.c -O3 -o ../../bin/main timer.o hough.o canny.o image.o bmp.o convolution.o
+gcc ../../src/main.c -O3 -o ../../bin/edges timer.o hough.o canny.o image.o bmp.o convolution.o
